b926c389aa27362cce9709b6614e90ab158ca2ec
pedro <pedro.moreno@aluno.ufabc.edu.br>
2023-03-12 19:58:21 -0300

    Added sorting util and minor fixes

--- process.c
+++ process.c
@@ -3,6 +3,22 @@
 #include "process.h"
 
 void print_proc(Process p) {
-    printf("%s, %d, %d - Arrival time: %d\n",
-        p.name, p.priority, p.cpu_burst, p.arrival);
+    printf("%s, %d, %d - Waiting time: %d\n",
+        p.name, p.priority, p.cpu_burst, p.waiting_time);
+}
+
+void insertion_sort_by_cpu_burst(Process procs[], int procs_len) {
+    for(int j = 1; j < procs_len; j++)
+    {
+        Process chave = procs[j];
+        int i = j - 1;
+
+        // insert chave in v[0..j-1 = i]
+        while(i >= 0 && chave.cpu_burst < procs[i].cpu_burst)
+        {
+            procs[i + 1] = procs[i];
+            i--;
+        }
+        procs[i + 1] = chave;
+    }
 }
